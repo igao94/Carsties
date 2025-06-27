915d8438fa129a1537645b7b6314e9ac99b0866a
igao94 <igao998@gmail.com>
2025-06-27 11:57:42 +0200

    Add endpoint to search items by make, model, or color

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Entites;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string? searchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(i => i.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return result;
+    }
+}
