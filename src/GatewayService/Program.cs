68df14a0baecec57881d970ffdef045940848d61
igao94 <igao998@gmail.com>
2025-07-04 12:36:27 +0200

    Configure YARP routes and clusters in GatewayService

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
