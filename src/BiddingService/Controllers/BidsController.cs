515a74f182a01c35c79f2edf410bb285856ff55f
igao94 <igao998@gmail.com>
2025-07-06 10:41:23 +0200

    Add endpoint to place a bid on an auction

---
+++ src/BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,63 @@
+﻿using BiddingService.Entites;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BidsController : ControllerBase
+{
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount)
+    {
+        var auction = await DB.Find<Auction>().OneAsync(auctionId);
+
+        if (auction is null)
+        {
+            return NotFound();
+        }
+
+        if (auction.Seller == User.Identity?.Name)
+        {
+            return BadRequest("You cannot bid on your own auction.");
+        }
+
+        var bid = new Bid
+        {
+            AuctionId = auction.ID,
+            Bidder = User.Identity?.Name!,
+            Amount = amount
+        };
+
+        if (auction.AuctionEnd < DateTime.UtcNow)
+        {
+            bid.BidStatus = BidStatus.Finished;
+        }
+        else
+        {
+            var highBid = await DB.Find<Bid>()
+                .Match(b => b.AuctionId == auction.ID)
+                .Sort(b => b.Descending(x => x.Amount))
+                .ExecuteFirstAsync();
+
+            if (highBid is not null && amount > highBid.Amount || highBid is null)
+            {
+                bid.BidStatus = amount > auction.ReservePrice
+                    ? BidStatus.Accepted
+                    : BidStatus.AcceptedBelowReserve;
+            }
+
+            if (highBid is not null && amount <= highBid.Amount)
+            {
+                bid.BidStatus = BidStatus.TooLow;
+            }
+        }
+
+        await DB.SaveAsync(bid);
+
+        return Ok(bid);
+    }
+}
