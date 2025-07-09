9079577d4422eea11abbe386be22a5fa9296e5a3
igao94 <igao998@gmail.com>
2025-07-09 09:26:13 +0200

    Add NotificationService

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,17 @@
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+app.Run();
